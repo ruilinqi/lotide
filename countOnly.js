20186aa84e678284ab5a3a41c32e1deeb758bc6b
ruilinqi <ruilinqi7@gmail.com>
2022-11-16 20:08:31 +0000

    Add new files

---
+++ countOnly.js
@@ -0,0 +1,35 @@
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+
+  for (const item of allItems) {
+    console.log(item);
+    
+    // inside the loop, 
+    // increment the counter for each item:
+    //   set a property with that string key to:
+    //     the value that was already there (or zero if nothing there) with 1 added to it.
+    if (results[item]) {
+      results[item] += 1;
+    } else {
+      results[item] = 1;
+    }
+  }
+  return results;
+}
+
+// Test Code
+const allItems = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+const itemsToCount = 'Joe';
+console.log(countOnly(allItems, itemsToCount));
