9c49667ecc01e32bb4004e45a0341f10cedda990
ruilinqi <ruilinqi7@gmail.com>
2022-11-16 02:20:11 +0000

    Add exsting files

--- assertEqual.js
+++ assertEqual.js
@@ -6,3 +6,5 @@ const assertEqual = function(actual, expected) {
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
+
+//assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
---
+++ head.js
@@ -0,0 +1,13 @@
+/**
+ *  Return the head of an array.
+ * if empty array => return 'undefined'
+ */
+const head = function(array) {
+  return array[0];
+}
+
+//Test code
+/**
+ * assertEqual(head([5,6,7]), 5);
+ * assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+ */
