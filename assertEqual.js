4dbd0e4fb50f0ea1e6c8805922c0c46a1fe3a5bb
ruilinqi <ruilinqi7@gmail.com>
2022-11-21 20:04:57 +0000

    Refactored assertEqual.js

--- assertEqual.js
+++ assertEqual.js
@@ -14,9 +14,3 @@ const assertEqual = function (actual, expected){
 
 module.exports = assertEqual;
 
-// TEST CODE
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // eqArray => true, should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false)// eqArray => false, should PASS
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // eqArray=> true, should PASS
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // eqArray => false, assertEqual should PASS
