efecfa133db68c21e6b46da9b158c62e569d2064
ruilinqi <ruilinqi7@gmail.com>
2022-11-21 23:57:08 +0000

    Refactored middle.js

--- middle.js
+++ middle.js
@@ -2,30 +2,29 @@
 const middle = function(array) {
   let result = [];
   if (array.length > 2){
+    //For arrays with odd number of elements, an array containing a single middle element should be returned
     if (array.length % 2 === 1){
-      return array.slice((array.length - 1) / 2, (array.length - 1) / 2 + 1);
+      return array.slice(array.length / 2, array.length / 2 + 1);
     }
+    //For arrays with an even number of elements, an array containing the two elements in the middle should be returned
     if (array.length % 2 === 0){
       return array.slice(array.length / 2 - 1, array.length / 2 + 1);
       //result = [(array.length / 2 - 1), (array.length /2)];
     }
   }
+  //For arrays with one or two elements, there is no middle. Return an empty array
   return [];
 }
-// TEST CODE
-//For arrays with one or two elements, there is no middle. Return an empty array
-middle([1]) // => []
-middle([1, 2]) // => []
 
-console.log(middle([1]));
-console.log(middle([1, 2]));
-//For arrays with odd number of elements, an array containing a single middle element should be returned
-middle([1, 2, 3]) // => [2]
-middle([1, 2, 3, 4, 5]) // => [3]
+module.exports = middle;
 
-console.log(middle([1, 2, 3]));
-//For arrays with an even number of elements, an array containing the two elements in the middle should be returned
-middle([1, 2, 3, 4]) // => [2, 3]
-middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
-console.log(middle([1, 2, 3, 4]));
-console.log(middle([1, 2, 3, 4, 5, 6]));
+//Test Code
+// console.log(middle([1]));
+// console.log(middle([1, 2]));
+
+// console.log(middle([1, 2, 3])); // => [2]
+// console.log(middle([1, 2, 3, 4, 5])); // => [3]
+
+
+// console.log(middle([1, 2, 3, 4])); // => [2, 3]
+// console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
