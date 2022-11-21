d97c78a9bd5c3f632698a1ccfd7ca0e158f2ef84
ruilinqi <ruilinqi7@gmail.com>
2022-11-21 20:09:16 +0000

    Refactored the haed.js file

--- head.js
+++ head.js
@@ -5,9 +5,4 @@
 const head = function(array) {
   return array[0];
 }
-
-//Test code
-/**
- * assertEqual(head([5,6,7]), 5);
- * assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
- */
+module.exports = head;
