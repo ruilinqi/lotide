6347bf6f5fe7f0f2039792612c757f9efc7acf70
ruilinqi <ruilinqi7@gmail.com>
2022-11-22 07:11:12 +0000

    Refactored assertEqual.js

--- assertEqual.js
+++ assertEqual.js
@@ -2,15 +2,15 @@
  * Use assertEqual to write test cases for various scenarios.
  */
 // requiring eqArrays function
-const eqArrays = require('./eqArrays');
+//const eqArrays = require('./eqArrays');
 
-const assertEqual = function (actual, expected){
-  if (actual === expected){
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  }else {
+  } else {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
-}
+};
 
 module.exports = assertEqual;
 
