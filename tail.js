ec3bb292e1b18a1687306e2eab49a218799ea9b4
ruilinqi <ruilinqi7@gmail.com>
2022-11-22 07:15:40 +0000

    Refactored tail.js

--- tail.js
+++ tail.js
@@ -3,8 +3,8 @@
  *  Tail: everything except for the first item (head) of the provided array.
  *  if empty array => return 'undefined'
  */
- const tail = function(array) {
+const tail = function(array) {
   return array.slice(1, array.length); //1, array.length-1
-}
+};
 
 module.exports = tail;
