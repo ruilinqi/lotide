9be314ed59e4fdc2edce42f3543a58c91f153162
ruilinqi <ruilinqi7@gmail.com>
2022-11-22 07:14:12 +0000

    Refactored countLetters.js and  countOnly.js

--- countLetters.js
+++ countLetters.js
@@ -4,13 +4,12 @@
 const countLetters = function(str) {
   let output = {};
   for (let character of str.split(' ').join('')) {
-    if (output[character]){ //check if output at character(ex: i) has anything //if the computer wrote anything inside
+    if (output[character]) { //check if output at character(ex: i) has anything //if the computer wrote anything inside
       output[character]++; //if true => output at this character + 1
-    }else{
+    } else {
       output[character] = 1;//if not => output of the character is 1
     }
   }
-
   return output;
 };
 
--- countOnly.js
+++ countOnly.js
@@ -2,22 +2,22 @@
 // itemsToCount: an object specifying what to count
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
-
   for (const item of allItems) {
-    console.log(item);
-    
-    // inside the loop, 
+    // console.log(item);
+    // inside the loop,
     // increment the counter for each item:
     //   set a property with that string key to:
     //     the value that was already there (or zero if nothing there) with 1 added to it.
-    if (results[item]) {
-      results[item] += 1;
-    } else {
-      results[item] = 1;
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
     }
   }
   return results;
-}
+};
 
 // Test Code
 const allItems = [
@@ -31,5 +31,5 @@ const allItems = [
   "Fang",
   "Joe"
 ];
-const itemsToCount = 'Joe';
+const itemsToCount = {'Salima':true, 'Joe': true};
 console.log(countOnly(allItems, itemsToCount));
