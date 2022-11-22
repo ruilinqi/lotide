8c59e699c30e6fa02316f8ee974c72e45f884550
ruilinqi <ruilinqi7@gmail.com>
2022-11-22 07:22:22 +0000

    Edited middle.js

--- middle.js
+++ middle.js
@@ -1,20 +1,19 @@
 // ACTUAL FUNCTION
 const middle = function(array) {
-  let result = [];
-  if (array.length > 2){
+  if (array.length > 2) {
     //For arrays with odd number of elements, an array containing a single middle element should be returned
-    if (array.length % 2 === 1){
+    if (array.length % 2 === 1) {
       return array.slice(array.length / 2, array.length / 2 + 1);
     }
     //For arrays with an even number of elements, an array containing the two elements in the middle should be returned
-    if (array.length % 2 === 0){
+    if (array.length % 2 === 0) {
       return array.slice(array.length / 2 - 1, array.length / 2 + 1);
       //result = [(array.length / 2 - 1), (array.length /2)];
     }
   }
   //For arrays with one or two elements, there is no middle. Return an empty array
   return [];
-}
+};
 
 module.exports = middle;
 
