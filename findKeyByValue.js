42ae515bdbfcae82f47a4761c562f08c068a8ef9
ruilinqi <ruilinqi7@gmail.com>
2022-11-22 07:20:53 +0000

    Fixed error

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,39 +1,24 @@
-
-const assertEqual = function (actual, expected){
-  if (actual === expected){
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  }else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-}
-
+const assertEqual = require('./assertEqual');
 /**
  * scan the object and return the first key which contains the given value.
  * If no key with that given value is found, then it should return undefined.
- * 
- * @param {*} object 
- * @param {*} value 
+ * @param {*} object
+ * @param {*} value
  */
-const findKeyByValue = function (object, value){
-  for (let key in object){
+const findKeyByValue = function(object, value) {
+  for (let key in object) {
     console.log(object[key]);
-    if (value === object[key]){
-      console.log("Find a match!");
+    if (value === object[key]) {
+      return key;
     }
   }
-}
-// const findKeyByValue = function (object, value){
-//   for (let key of object){
-//     console.log(Object.keys(object));
-//   }
-// }
+};
 
 // Test Code
-const bestTVShowsByGenre = { 
-  sci_fi: "The Expanse",
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
   drama:  "The Wire"
 };
-
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
--- findkey.js
+++ findkey.js
@@ -1,11 +1,11 @@
 //const findKey = function(object, callback){
 const findKey = (object, callback) => {
-  for (var key in object){
-    if (callback(object[key])){ // when object[key] is "noma"
+  for (let key in object) {
+    if (callback(object[key])) { // when object[key] is "noma"
       return key; //print key
     }
   }
-}
+};
 
 let obj = {
   "Blue Hill": { stars: 1 },
@@ -14,7 +14,7 @@ let obj = {
   "elBulli":   { stars: 3 },
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
-}
+};
 
 console.log(findKey(obj, x => x.stars === 2));
 //                  object   callback
