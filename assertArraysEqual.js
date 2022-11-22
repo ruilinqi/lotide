b42f6cf7761ebf52736d480448b040f8580ed1ad
ruilinqi <ruilinqi7@gmail.com>
2022-11-22 07:10:42 +0000

    Refactored assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,15 @@
-const eqArrays = require("./eqArrays")
+const eqArrays = require("./eqArrays");
 
 /**
- * we will not be writing assertions (test code) for assertArraysEqual. That said, 
- * you can and should call the function below its declaration and visually ensure 
+ * we will not be writing assertions (test code) for assertArraysEqual. That said,
+ * you can and should call the function below its declaration and visually ensure
  * that the message printed to the console is accurate.
- * 
  */
-const assertArraysEqual = function(actual, expected){
-  if (eqArrays(actual, expected)){
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
-}
+};
 module.exports = assertArraysEqual;
